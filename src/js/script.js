49a99a2db811db1907ecc7097b4ed44b84e89f86
Gustavo <Gustavohiruo@gmail.com>
2025-09-29 11:33:50 -0300

    Declarando variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-alert("hello word")
+// alert("hello word")
+
+// declaração de variavel
+
+var nome = "Fiap";
+console.log(typeof nome)
+
+let nome1 = "10";
+console.log(typeof nome1)
+
+const nome2 = {};
+console.log(typeof nome2)
+
+let nome3 = [];
+console.log(typeof nome3)
+
+let nome4=true;
+console.log(typeof nome4)
